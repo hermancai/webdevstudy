fff30763e6efac87fa39c895ceb0fb1f18c6a91f
Herman Cai <hermancai3@gmail.com>
2024-03-09 21:09:33 -0800

    Add catch-all 404 page

--- app/[slug]/page.tsx
+++ app/[slug]/page.tsx
@@ -2,7 +2,7 @@ import parseMarkdown from "@/services/parseMarkdown";
 import getProperName from "@/services/getProperName";
 import Card from "@/components/Card";
 import getMarkdownFileNames from "@/services/getMarkdownFileNames";
-import Link from "next/link";
+import NotFoundPage from "@/components/NotFoundPage";
 
 // Routes are generated at build time based on files in the markdown directory
 export async function generateStaticParams() {
@@ -35,16 +35,6 @@ export default function Page({ params }: { params: { slug: string } }) {
             </div>
         );
     } catch (e) {
-        return (
-            <div className="max-w-6xl mx-auto flex flex-col gap-4">
-                <h1 className="text-3xl">Page Not Found</h1>
-                <Link
-                    href="/"
-                    className="py-2 px-4 rounded bg-neutral-800 hover:bg-neutral-700 transition-colors w-min whitespace-nowrap"
-                >
-                    Home Page
-                </Link>
-            </div>
-        );
+        return <NotFoundPage />;
     }
 }
---
+++ components/NotFoundPage.tsx
@@ -0,0 +1,15 @@
+import Link from "next/link";
+
+export default function NotFoundPage() {
+    return (
+        <div className="max-w-6xl mx-auto flex flex-col gap-4">
+            <h1 className="text-3xl">Page Not Found</h1>
+            <Link
+                href="/"
+                className="py-2 px-4 rounded bg-neutral-800 hover:bg-neutral-700 transition-colors w-min whitespace-nowrap"
+            >
+                Home Page
+            </Link>
+        </div>
+    );
+}
