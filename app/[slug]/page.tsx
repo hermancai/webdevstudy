c19cb3d2eafd3890675decd1b1090f95735bae50
Herman Cai <hermancai3@gmail.com>
2024-03-09 21:05:04 -0800

    Catch error in case of no matching markdown file

--- app/[slug]/page.tsx
+++ app/[slug]/page.tsx
@@ -2,6 +2,7 @@ import parseMarkdown from "@/services/parseMarkdown";
 import getProperName from "@/services/getProperName";
 import Card from "@/components/Card";
 import getMarkdownFileNames from "@/services/getMarkdownFileNames";
+import Link from "next/link";
 
 // Routes are generated at build time based on files in the markdown directory
 export async function generateStaticParams() {
@@ -21,16 +22,29 @@ export async function generateMetadata({
 
 export default function Page({ params }: { params: { slug: string } }) {
     const { slug } = params;
-    const cards = parseMarkdown(slug);
-
-    return (
-        <div className="max-w-6xl mx-auto flex flex-col gap-4">
-            <h1 className="font-mono text-2xl">{getProperName(slug)}</h1>
-            <div className="flex flex-col gap-6">
-                {cards.map((card, i) => {
-                    return <Card key={i} content={card} number={i + 1} />;
-                })}
+    try {
+        const cards = parseMarkdown(slug);
+        return (
+            <div className="max-w-6xl mx-auto flex flex-col gap-4">
+                <h1 className="font-mono text-2xl">{getProperName(slug)}</h1>
+                <div className="flex flex-col gap-6">
+                    {cards.map((card, i) => {
+                        return <Card key={i} content={card} number={i + 1} />;
+                    })}
+                </div>
             </div>
-        </div>
-    );
+        );
+    } catch (e) {
+        return (
+            <div className="max-w-6xl mx-auto flex flex-col gap-4">
+                <h1 className="text-3xl">Page Not Found</h1>
+                <Link
+                    href="/"
+                    className="py-2 px-4 rounded bg-neutral-800 hover:bg-neutral-700 transition-colors w-min whitespace-nowrap"
+                >
+                    Home Page
+                </Link>
+            </div>
+        );
+    }
 }
